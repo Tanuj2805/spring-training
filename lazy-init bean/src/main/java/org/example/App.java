be99b7c08fca9ebd64c999486bb0bf6dffbacfb5
Tanuj2805 <tanujkeshattiwar@gmail.com>
2024-12-29 21:23:01 +0530

    Added commenrts for understanding

--- lazy-init bean/src/main/java/org/example/App.java
+++ lazy-init bean/src/main/java/org/example/App.java
@@ -16,8 +16,8 @@ public class App
 
         Coder a = (Coder) con.getBean("code");
         //Here Coder class is depend on one class and we are passing laptop object using constructor
-        // As the both the class is declared lazy init so only when it is neede at that time only it will create the object
-        //Here desktop is not needed so it doesnt creat it
+        // As the both the class is declared lazy init so only when it is need at that time only it will create the object
+        //Here desktop is not needed so it dosen't create it
         //To more understand compare with Autowire file
         a.coding();
 
