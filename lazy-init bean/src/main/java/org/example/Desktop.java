12ae11a17af97c3517efbc5b225d09ffad93904c
Tanuj2805 <tanujkeshattiwar@gmail.com>
2024-12-29 21:11:32 +0530

    Added Lazy-init bean file

---
+++ lazy-init bean/src/main/java/org/example/Desktop.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Desktop implements Machine
+{
+
+
+    public Desktop()
+    {
+
+        System.out.println("Desktop Object Is Created");
+    }
+
+    public Desktop(Machine system)
+    {
+        System.out.println("Desktop Object Is Created");
+
+    }
+
+    @Override
+    public void compile()
+    {
+        System.out.println("Desktop Object Is Created");
+    }
+
+
+}
---
+++ lazy-init bean/src/main/java/org/example/Laptop.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Laptop implements Machine
+{
+
+    public Laptop() {
+
+        System.out.println("Laptop Object Is Created");
+
+    }
+
+    @Override
+    public void compile()
+    {
+
+
+        System.out.println("Compiling using laptop");
+
+
+    }
+}
