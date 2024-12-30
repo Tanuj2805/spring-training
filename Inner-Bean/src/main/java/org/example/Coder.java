377d48e73b9ac3a3e2fc67f1cabdc67aa44baade
Tanuj2805 <tanujkeshattiwar@gmail.com>
2024-12-30 14:55:04 +0530

    Done with basic logic desigining

--- Inner-Bean/src/main/java/org/example/Coder.java
+++ Inner-Bean/src/main/java/org/example/Coder.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Coder {
+
+    Machine system;
+
+    public Coder() {
+        System.out.println("Coder Object Is Created");
+    }
+
+    public Machine getSystem() {
+        return system;
+    }
+
+    public void setSystem(Machine system) {
+        this.system = system;
+    }
+
+    public void code()
+    {
+        System.out.println("Coding...");
+        system.compile();
+    }
 }
--- Inner-Bean/src/main/java/org/example/Laptop.java
+++ Inner-Bean/src/main/java/org/example/Laptop.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class Laptop {
+public class Laptop implements Machine
+{
+
+
+    public Laptop() {
+
+        System.out.println("Laptop Object Is Created");
+
+    }
+
+    @Override
+    public void compile() {
+
+        System.out.println("Compiling Using Laptop");
+
+    }
 }
