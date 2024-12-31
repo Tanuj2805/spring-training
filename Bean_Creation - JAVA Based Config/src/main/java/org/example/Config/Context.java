5904890406cbe29cebb14a77b3bbe0f8a7d08a41
Tanuj2805 <tanujkeshattiwar@gmail.com>
2024-12-31 15:18:32 +0530

    Started With JAVA Based Config

--- Bean_Creation - JAVA Based Config/src/main/java/org/example/Config/Context.java
+++ Bean_Creation - JAVA Based Config/src/main/java/org/example/Config/Context.java
@@ -1,4 +1,16 @@
 package org.example.Config;
 
+import org.example.Student;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Context {
+
+    @Bean
+    public Student st()
+    {
+        return new Student();
+    }
 }
