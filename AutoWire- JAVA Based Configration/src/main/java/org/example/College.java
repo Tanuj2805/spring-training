400b49e1d018cf94e707021402242205fe9ef41f
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-01-01 14:55:22 +0530

    Added Required Classes

---
+++ AutoWire- JAVA Based Configration/src/main/java/org/example/College.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class College implements Education {
+}
---
+++ AutoWire- JAVA Based Configration/src/main/java/org/example/Config/Context.java
@@ -0,0 +1,8 @@
+package org.example.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Context {
+}
