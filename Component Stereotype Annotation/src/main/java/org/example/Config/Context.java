e247556bc44e8249a799f61d93499f23c3875555
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-01-02 15:39:06 +0530

    Done With Component Annotation

---
+++ Component Stereotype Annotation/src/main/java/org/example/Config/Context.java
@@ -0,0 +1,12 @@
+package org.example.Config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example") //IT WILL SCAN FOR COMPONENT IN org.example Package
+public class Context {
+
+
+}
---
+++ Component Stereotype Annotation/src/main/java/org/example/Student.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    String name;
+    int rn;
+
+    public Student() {
+
+        System.out.println("STUDENT OBJECT CREATED");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRn() {
+        return rn;
+    }
+
+    public void setRn(int rn) {
+        this.rn = rn;
+    }
+
+    public void getdata()
+    {
+        System.out.println("NAME: "+name);
+        System.out.println("Roll No: "+rn);
+    }
+}
