7850c282010a62545dba82a3e749e384c1e08fd9
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-01-03 15:48:38 +0530

    Done

--- Scope Annotation/src/main/java/org/example/App.java
+++ Scope Annotation/src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import io.micrometer.observation.Observation;
+import org.example.config.context;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,8 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        ApplicationContext con = new AnnotationConfigApplicationContext(context.class);
+        Student st = con.getBean(Student.class);
+        st.getdata();
     }
 }
--- Scope Annotation/src/main/java/org/example/Student.java
+++ Scope Annotation/src/main/java/org/example/Student.java
@@ -3,7 +3,7 @@ package org.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class Student {
 
 
