1f7050b209bca378c9d24b8dcd77f4518542bfbd
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-01-03 15:42:26 +0530

    Added Required Classes

---
+++ Scope Annotation/src/main/java/org/example/Student.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+public class Student {
+
+
+
+
+    @Value("Tanuj")
+    String Name;
+
+    @Value("VIT")
+    String Sname;
+
+    @Value("34")
+    int Roll;
+
+
+
+    public Student() {
+
+        System.out.println("Student Object Created");
+    }
+
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSname() {
+        return Sname;
+    }
+
+    public void setSname(String sname) {
+        Sname = sname;
+    }
+
+    public int getRoll() {
+        return Roll;
+    }
+
+    public void setRoll(int roll) {
+        Roll = roll;
+    }
+
+    public void getdata()
+    {
+        System.out.println("Name: "+Name);
+        System.out.println("School/College Name: "+Sname);
+        System.out.println("ID code: "+Roll);
+
+    }
+}
---
+++ Scope Annotation/src/main/java/org/example/config/context.java
@@ -0,0 +1,16 @@
+package org.example.config;
+
+
+import org.example.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class context {
+
+
+
+
+}
